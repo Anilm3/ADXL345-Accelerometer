ab0a02436d9d8ab273ee70c1e294ab9f7abb4a81
Anilm3 <Anilm3>
2012-09-19 19:41:19 +0100

    First code upload

---
+++ ADXL345.cpp
@@ -0,0 +1,73 @@
+#include <Wire.h>
+#include <ADXL345.h>
+
+int ADXL345::writeRegister(byte reg_addr, int nbytes, byte *buffer)
+{
+	int written_bytes;
+	
+	Wire.beginTransmission(ADXL345_ADDRESS);
+	Wire.write(reg_addr);
+	written_bytes = Wire.write(buffer, nbytes);
+	Wire.endTransmission();
+	
+	return written_bytes;
+}
+
+int ADXL345::readRegister(byte reg_addr, int nbytes, byte *buffer)
+{
+	int idx = 0;
+
+	Wire.beginTransmission(ADXL345_ADDRESS);
+	Wire.write(reg_addr);
+	Wire.endTransmission(); 
+	
+	Wire.requestFrom(ADXL345_ADDRESS, nbytes);
+
+	while(Wire.available() && idx < nbytes)
+	{ 
+		buffer[idx++] = Wire.read();
+	}
+	
+	return idx;
+}
+
+ADXL345::ADXL345()
+{
+	Wire.begin();
+	
+	zG[0] = -20;
+	zG[1] =  15;
+	zG[2] = -23}
+}
+
+void ADXL345::begin()
+{
+	byte data = 0x08;
+	writeRegister(ADXL345_POWER_CTL, 1, &data);
+}
+
+
+void ADXL345::read(double *x, double *y, double *z)
+{
+	byte buffer[6];
+
+	readRegister(ADXL345_DATAX0, 6, buffer);
+	
+	*x = ((buffer[0] + (buffer[1] << 8)) - zG[0])/256.0;
+	*y = ((buffer[2] + (buffer[3] << 8)) - zG[1])/256.0;
+	*z = ((buffer[4] + (buffer[5] << 8)) - zG[2])/256.0;
+}
+
+void ADXL345::end()
+{
+	byte data = 0x00;
+	writeRegister(ADXL345_POWER_CTL, 1, &data);
+}
+
+void setZeroG(double x, double y, double z)
+{
+	zG[0] = x*256.0;
+	zG[1] = y*256.0;
+	zG[2] = z*256.0;
+}
+
---
+++ ADXL345.h
@@ -0,0 +1,56 @@
+#ifndef _ADXL345_H
+#define _ADXL345_H
+
+#include <Arduino.h> 
+
+/* ADXL345 Registers */
+#define ADXL345_DEVID			0x00
+#define ADXL345_THRESH_TAP		0x1D
+#define ADXL345_OFSX			0x1E
+#define ADXL345_OFSY			0x1F
+#define ADXL345_OFSZ			0x20
+#define ADXL345_DUR				0x21
+#define ADXL345_Latent			0x22
+#define ADXL345_Window			0x23
+#define ADXL345_THRESH_ACT		0x24
+#define ADXL345_THRESH_INACT	0x25
+#define ADXL345_TIME_INACT		0x26
+#define ADXL345_ACT_INACT_CTL	0x27
+#define ADXL345_THRESH_FF		0x28
+#define ADXL345_TIME_FF			0x29
+#define ADXL345_TAP_AXES		0x2A
+#define ADXL345_ACT_TAP_STATUS	0x2B
+#define ADXL345_BW_RATE			0x2C
+#define ADXL345_POWER_CTL		0x2D
+#define ADXL345_INT_ENABLE		0x2E
+#define ADXL345_INT_MAP			0x2F
+#define ADXL345_INT_SOURCE		0x30
+#define ADXL345_DATA_FORMAT		0x31
+#define ADXL345_DATAX0			0x32
+#define ADXL345_DATAX1			0x33
+#define ADXL345_DATAY0			0x34
+#define ADXL345_DATAY1			0x35
+#define ADXL345_DATAZ0			0x36
+#define ADXL345_DATAZ1			0x37
+#define ADXL345_FIFO_CTL		0x38
+#define ADXL345_FIFO_STATUS		0x39
+
+#define ADXL345_ADDRESS			0xA7 >> 1
+
+// All functions return -1 on error and 0 on success
+class ADXL345
+{
+	int zG[3];
+	int readRegister(byte reg_addr, int nbytes, byte *buffer);
+	int writeRegister(byte reg_addr, int nbytes, byte *buffer);
+	
+	public:
+	ADXL345();
+	void begin();
+	void read(double *x, double *y, double *z);
+	void end();
+	
+	void setZeroG(double x, double y, double z);
+};
+
+#endif // _ADXL345_H
