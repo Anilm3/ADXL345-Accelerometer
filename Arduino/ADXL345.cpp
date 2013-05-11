fe6c32cc2f22bc51cb368f4a5147ad43fca73936
Anil <anil.mmm@gmail.com>
2013-05-11 18:16:27 +0100

    Now using scoped enums and added setrange function

--- Arduino/ADXL345.cpp
+++ Arduino/ADXL345.cpp
@@ -10,13 +10,13 @@
 *********************************************************************************/
 
 #include <ADXL345.h>
-#include <Wire.h>"
+#include <Wire.h>
 
 int ADXL345::writeRegister(byte reg_addr, int nbytes, byte *buffer)
 {
 	int written_bytes;
 	
-	Wire.beginTransmission(ADXL345_ADDRESS);
+	Wire.beginTransmission(ADXL345::ADDRESS);
 	Wire.write(reg_addr);
 	written_bytes = Wire.write(buffer, nbytes);
 	Wire.endTransmission();
@@ -28,11 +28,11 @@ int ADXL345::readRegister(byte reg_addr, int nbytes, byte *buffer)
 {
 	int idx = 0;
 
-	Wire.beginTransmission(ADXL345_ADDRESS);
+	Wire.beginTransmission(ADXL345::ADDRESS);
 	Wire.write(reg_addr);
 	Wire.endTransmission(); 
 	
-	Wire.requestFrom(ADXL345_ADDRESS, nbytes);
+	Wire.requestFrom(ADXL345::ADDRESS, nbytes);
 
 	while(Wire.available() && idx < nbytes)
 	{ 
@@ -54,20 +54,20 @@ ADXL345::ADXL345()
 void ADXL345::begin()
 {
 	byte data = 0x08;
-	writeRegister(ADXL345_POWER_CTL, 1, &data);
+	writeRegister(ADXL345::POWER_CTL, 1, &data);
 }
 
 void ADXL345::end()
 {
 	byte data = 0x00;
-	writeRegister(ADXL345_POWER_CTL, 1, &data);
+	writeRegister(ADXL345::POWER_CTL, 1, &data);
 }
 
 void ADXL345::read(double *x, double *y, double *z)
 {
 	byte buffer[6];
 
-	readRegister(ADXL345_DATAX0, 6, buffer);
+	readRegister(ADXL345::DATAX0, 6, buffer);
 	
 	*x = ((buffer[0] + (buffer[1] << 8)) - zG[0])/256.0;
 	*y = ((buffer[2] + (buffer[3] << 8)) - zG[1])/256.0;
@@ -78,13 +78,26 @@ void ADXL345::read(int *x, int *y, int *z)
 {
 	byte buffer[6];
 
-	readRegister(ADXL345_DATAX0, 6, buffer);
+	readRegister(ADXL345::DATAX0, 6, buffer);
 	
 	*x = buffer[0] + (buffer[1] << 8);
 	*y = buffer[2] + (buffer[3] << 8);
 	*z = buffer[4] + (buffer[5] << 8);
 }
 
+
+void ADXL345::setRange(range_t range)
+{	
+	switch(range)
+	{
+		case RANGE_16G:
+		case RANGE_8G:
+		case RANGE_4G:
+		case RANGE_2G:
+			writeRegister(ADXL345::DATA_FORMAT, 1, (byte *)&range);
+			break;
+	}
+}
 
 void ADXL345::setZeroG(double x, double y, double z)
 {
--- Arduino/ADXL345.h
+++ Arduino/ADXL345.h
@@ -14,50 +14,60 @@
 
 #include <Arduino.h>
 
-/* ADXL345 Registers */
-#define ADXL345_DEVID			0x00
-#define ADXL345_THRESH_TAP		0x1D
-#define ADXL345_OFSX			0x1E
-#define ADXL345_OFSY			0x1F
-#define ADXL345_OFSZ			0x20
-#define ADXL345_DUR				0x21
-#define ADXL345_Latent			0x22
-#define ADXL345_Window			0x23
-#define ADXL345_THRESH_ACT		0x24
-#define ADXL345_THRESH_INACT	0x25
-#define ADXL345_TIME_INACT		0x26
-#define ADXL345_ACT_INACT_CTL	0x27
-#define ADXL345_THRESH_FF		0x28
-#define ADXL345_TIME_FF			0x29
-#define ADXL345_TAP_AXES		0x2A
-#define ADXL345_ACT_TAP_STATUS	0x2B
-#define ADXL345_BW_RATE			0x2C
-#define ADXL345_POWER_CTL		0x2D
-#define ADXL345_INT_ENABLE		0x2E
-#define ADXL345_INT_MAP			0x2F
-#define ADXL345_INT_SOURCE		0x30
-#define ADXL345_DATA_FORMAT		0x31
-#define ADXL345_DATAX0			0x32
-#define ADXL345_DATAX1			0x33
-#define ADXL345_DATAY0			0x34
-#define ADXL345_DATAY1			0x35
-#define ADXL345_DATAZ0			0x36
-#define ADXL345_DATAZ1			0x37
-#define ADXL345_FIFO_CTL		0x38
-#define ADXL345_FIFO_STATUS		0x39
-
-#define ADXL345_ADDRESS			0xA7 >> 1
-
 // All functions return -1 on error and 0 on success
 class ADXL345
 {
 	private:
+		/* ADXL345 Registers */
+		enum info_t 
+		{
+			DEVID          = 0x00,
+			THRESH_TAP     = 0x1D,
+			OFSX           = 0x1E,
+			OFSY           = 0x1F,
+			OFSZ           = 0x20,
+			DUR            = 0x21,
+			Latent         = 0x22,
+			Window         = 0x23,
+			THRESH_ACT     = 0x24,
+			THRESH_INACT   = 0x25,
+			TIME_INACT     = 0x26,
+			ACT_INACT_CTL  = 0x27,
+			THRESH_FF      = 0x28,
+			TIME_FF        = 0x29,
+			TAP_AXES       = 0x2A,
+			ACT_TAP_STATUS = 0x2B,
+			BW_RATE        = 0x2C,
+			POWER_CTL      = 0x2D,
+			INT_ENABLE     = 0x2E,
+			INT_MAP        = 0x2F,
+			INT_SOURCE     = 0x30,
+			DATA_FORMAT    = 0x31,
+			DATAX0         = 0x32,
+			DATAX1         = 0x33,
+			DATAY0         = 0x34,
+			DATAY1         = 0x35,
+			DATAZ0         = 0x36,
+			DATAZ1         = 0x37,
+			FIFO_CTL       = 0x38,
+			FIFO_STATUS    = 0x39,
+			ADDRESS        = 0xA7 >> 1
+		};
+
+
 		int zG[3];
 		int readRegister(byte reg_addr, int nbytes, byte *buffer);
 		int writeRegister(byte reg_addr, int nbytes, byte *buffer);
 	
 	public:
-	
+		enum range_t
+		{
+			RANGE_16G = 0x0B,
+			RANGE_8G  = 0x0A,
+			RANGE_4G  = 0x09,
+			RANGE_2G  = 0x08
+		};
+
 		ADXL345();
 	
 		void begin();
@@ -69,7 +79,9 @@ class ADXL345
 		//Raw reading
 		void read(int *x, int *y, int *z);
 	
-	
+		//Unit must be res_t
+		void setRange(range_t range);
+
 		//Unit must be G
 		void setZeroG(double x, double y, double z);
 	
